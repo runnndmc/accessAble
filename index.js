b8bd8bdad7f3dc645ff485a85ea652b5882593f0
DaynaChristison <daynamarie1743@gmail.com>
2022-05-28 15:02:31 -0400

    new pakages from express and trying to get js to run

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+
+console.log(process.argv);
+//https://devdojo.com/bo-iliev/how-to-write-your-first-nodejs-script
+
 const http = require('http')
+const express = require('express')
+const app = express()
 const fs = require('fs')
+const { errorMonitor } = require('events')
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+
 const server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/html"})
-    fs.readFile('index.html', function(error, data){
-        if(error){
-            res.writeHead(404)
-            res.write("Error: file not found")
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
+    if (req.url.indexOf('html') != 1 ){
+        fs.readFile(__dirname + '/index.html', function(error, data){
+            if(error){
+                res.writeHead(404)
+                res.write("Error: file not found")
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"})
+                res.write(data)
+                res.end('html page')
+            }
+        })
+    }
 })
 
 server.listen(port, function(error){
